d86d32972eefd772b435811fa93957c8c0ce5167
jaihee <jaihee0402@gmail.com>
2016-03-30 23:57:15 +0900

    fixed error

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -410,7 +410,7 @@ thread_set_nice (int nice)
   enum intr_level old_level = intr_disable ();
   struct thread *t = thread_current();
 
-  printf("thread : %s : %d %d\n", t->name, t->nice,nice);
+  printf("thread : %s : %d %d %d\n", t->name, t->nice,nice, t->priority);
   int recent = t->recent_cpu;
   int coeff = (((int64_t)(2*load_avg)) * FP )/ (2*load_avg + FP);
   t -> recent_cpu = ((int64_t)coeff) * recent / FP + nice * FP;
@@ -418,7 +418,7 @@ thread_set_nice (int nice)
   int priority = calc_priority(t->recent_cpu, nice);
   t->nice = nice;
   t->priority = priority;
-  printf("thread : %s : %d\n", t->name, t->nice);
+  printf("thread : %s : %d %d\n", t->name, t->nice, t->priority);
 
   if( list_empty(&ready_list) == false)
   {
