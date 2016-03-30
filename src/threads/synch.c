71ef959fb3a26db0c57ea80c3d1fa2470b6eaaa4
jaihee <jaihee0402@gmail.com>
2016-03-30 23:18:42 +0900

    fixed error

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -31,6 +31,11 @@
 #include <string.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+///WHERE WE ADDED/////////
+bool sema_less_func(const struct list_elem *a,
+                    const struct list_elem *b,
+                    void *aux);
+///WHERE WE ADDED END/////
 
 /* Initializes semaphore SEMA to VALUE.  A semaphore is a
    nonnegative integer along with two atomic operators for
@@ -322,10 +327,14 @@ cond_signal (struct condition *cond, struct lock *lock UNUSED)
   ASSERT (lock != NULL);
   ASSERT (!intr_context ());
   ASSERT (lock_held_by_current_thread (lock));
-
-  if (!list_empty (&cond->waiters)) 
+  
+  ///WHERE WE ADDED/////////
+  if (!list_empty (&cond->waiters)) {
+    list_sort(&cond->waiters, (list_less_func *) &sema_less_func, NULL);
     sema_up (&list_entry (list_pop_front (&cond->waiters),
                           struct semaphore_elem, elem)->semaphore);
+  }
+  ///WHERE WE ADDED END/////
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
@@ -343,3 +352,28 @@ cond_broadcast (struct condition *cond, struct lock *lock)
   while (!list_empty (&cond->waiters))
     cond_signal (cond, lock);
 }
+
+/************************************************************************
+* FUNCTION : sema_less_func                                             *
+* Input : struct list_elem *, struct list_elem *, void *                *
+* Output : boolean                                                      *
+* Purporse : compare semaphore race condition                           * 
+************************************************************************/
+/* ADDED FUNCTIONS */
+bool
+sema_less_func(const struct list_elem *a, const struct list_elem *b, void *aux)
+{
+  struct semaphore_elem *sema_elem_a = list_entry(a, struct semaphore_elem, elem);
+  struct semaphore_elem *sema_elem_b = list_entry(b, struct semaphore_elem, elem);
+
+  struct thread *thread_a = list_entry(list_front(&sema_elem_a->semaphore.waiters),
+                                       struct thread, elem);
+  struct thread *thread_b = list_entry(list_front(&sema_elem_b->semaphore.waiters),
+                                       struct thread, elem);
+
+  if ( thread_a->priority > thread_b->priority)
+    return true;
+  else return false;
+
+}
+
