d7139b2d6615f1c0517f2f567d20f62c50993d8e
jaihee <jaihee0402@gmail.com>
2016-04-10 01:04:41 +0900

    priority donation tried

--- src/tests/threads/priority-donate-one.c
+++ src/tests/threads/priority-donate-one.c
@@ -46,7 +46,6 @@ static void
 acquire1_thread_func (void *lock_) 
 {
   struct lock *lock = lock_;
-  msg ("acquire1: try the lock");
 
   lock_acquire (lock);
   msg ("acquire1: got the lock");
@@ -58,7 +57,7 @@ static void
 acquire2_thread_func (void *lock_) 
 {
   struct lock *lock = lock_;
-  msg ("acquire2: try the lock");
+
   lock_acquire (lock);
   msg ("acquire2: got the lock");
   lock_release (lock);
--- src/threads/synch.c
+++ src/threads/synch.c
@@ -285,11 +285,16 @@ lock_release (struct lock *lock)
   ASSERT (lock != NULL);
   ASSERT (lock_held_by_current_thread (lock));
   ///WHERE WE ADDED/////////
+      enum intr_level old_level;
+  old_level = intr_disable ();
+
   list_remove(&lock->elem);
   thread_set_priority(thread_current()->priority_rollback);
   ///WHERE WE ADDED END/////
   lock->holder = NULL;
   sema_up (&lock->semaphore);
+    intr_set_level (old_level);
+
 }
 
 /* Returns true if the current thread holds LOCK, false
