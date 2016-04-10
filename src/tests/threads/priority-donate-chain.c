9a09e88297c67097226bc33075ee098739be3000
jaihee <jaihee0402@gmail.com>
2016-04-10 21:58:59 +0900

    priority donation tried

--- src/tests/threads/priority-donate-chain.c
+++ src/tests/threads/priority-donate-chain.c
@@ -94,9 +94,9 @@ donor_thread_func (void *locks_)
   msg ("%s got lock", thread_name ());
 
   lock_release (locks->second);
-  msg ("%s should have priority %d. Actual priority: %d", 
+  msg ("%s should have priority %d. Actual priority: %d : %d ", 
         thread_name (), (NESTING_DEPTH - 1) * 3,
-        thread_get_priority ());
+        thread_get_priority (), thread_current()->priority);
 
   if (locks->first)
     lock_release (locks->first);
