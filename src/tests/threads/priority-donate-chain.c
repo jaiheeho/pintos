7c3c44b7f828b9b21982eb68a2d266d9629f475f
jaihee <jaihee0402@gmail.com>
2016-04-10 21:38:32 +0900

    priority donation tried

--- src/tests/threads/priority-donate-chain.c
+++ src/tests/threads/priority-donate-chain.c
@@ -100,6 +100,10 @@ donor_thread_func (void *locks_)
 
   if (locks->first)
     lock_release (locks->first);
+  
+  msg ("%s should have priority %d. Actual priority: %d", 
+      thread_name (), (NESTING_DEPTH - 1) * 3,
+      thread_get_priority ());
 
   msg ("%s finishing with priority %d.", thread_name (),
                                          thread_get_priority ());
