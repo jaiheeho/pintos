0f4a5b5f5fdd8cd0171010aa804158922b76c8d9
jaihee <jaihee0402@gmail.com>
2016-04-10 12:25:37 +0900

    priority donation tried

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -287,9 +287,7 @@ lock_release (struct lock *lock)
   lock->holder = NULL;
   if (!list_empty (&sema->waiters)) {
     list_sort(&sema->waiters, (list_less_func *) &priority_less_func, NULL);
-    printf("to pop : %s, %d, %d\n",to_pop->name, to_pop->priority, thread_get_priority());
     to_pop = list_entry (list_pop_front (&sema->waiters),struct thread, elem);
-    printf("to pop : %s, %d, %d\n",to_pop->name, to_pop->priority, thread_get_priority());
   }
   list_remove(&lock->elem);
   if (list_empty(&t->lock_holdings))
@@ -305,6 +303,8 @@ lock_release (struct lock *lock)
   if (to_pop != NULL)
     thread_unblock (to_pop);
   intr_set_level (old_level);
+  printf("to pop : %s, %d, %d\n",to_pop->name, to_pop->priority, thread_get_priority());
+
   ///WHERE WE ADDED END/////
 
 }
