b692c081b336c63829bc75a25da552289168d7fb
jaihee <jaihee0402@gmail.com>
2016-04-17 23:23:21 +0900

    implementing

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -31,6 +31,7 @@ process_execute (const char *file_name)
 {
   char *fn_copy;
   tid_t tid;
+  printf("process_execute :  %s to execute : %s\n", curr->name, file_name);
 
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
