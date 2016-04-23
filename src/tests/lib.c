f61e3488d06f23037abf3a6228e8bf56703e8fee
jaihee <jaihee0402@gmail.com>
2016-04-23 20:41:39 +0900

    implementing

--- src/tests/lib.c
+++ src/tests/lib.c
@@ -21,6 +21,7 @@ vmsg (const char *format, va_list args, const char *suffix)
   snprintf (buf, sizeof buf, "(%s) ", test_name);
   vsnprintf (buf + strlen (buf), sizeof buf - strlen (buf), format, args);
   strlcpy (buf + strlen (buf), suffix, sizeof buf - strlen (buf));
+  printf("in vmsg %s\n", buf);
   write (STDOUT_FILENO, buf, strlen (buf));
 }
 
