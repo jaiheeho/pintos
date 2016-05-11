e054f6b1ba577ce25b25f40fab8074cc8f34c944
jaihee <jaihee0402@gmail.com>
2016-05-12 01:42:10 +0900

    implementing

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -153,8 +153,6 @@ exit(int status)
   printf("%s: exit(%d)\n", thread_name(), status);
   struct thread *curr = thread_current();
   curr->exit_status=status;
-        //printf("exit %d\n", status);
-
   if (status == -1)
     sema_up(&filesys_global_lock);
   thread_exit();
@@ -221,8 +219,6 @@ bool
 remove (const char *file)
 {
   bool success;
-  if(invalid_addr((void*)file))
-    exit(-1);
   sema_down(&filesys_global_lock);
   success = filesys_remove(file);
   sema_up(&filesys_global_lock);
@@ -325,6 +321,14 @@ int read (int fd, void *buffer, unsigned length)
       sema_up(&filesys_global_lock);
       return -1;
     }
+    for (i = 0; i< length ; i++)
+    {
+      if (!put_user (((char*)buffer) + i , 1))
+      {
+        sema_up(&filesys_global_lock);
+        exit(-1);
+      }
+    }
     retval = file_read(file, buffer, length);
     sema_up(&filesys_global_lock);
   }
@@ -365,10 +369,7 @@ int write(int fd, const void *buffer, unsigned length)
         sema_up(&filesys_global_lock);
         return -1;
       }
-      //printf("write\n");
       retval = file_write(file, buffer, length);
-        //    printf("write\n");
-
       sema_up(&filesys_global_lock);
     }
 
@@ -533,4 +534,13 @@ bool invalid_addr(void* addr){
     return true;
 
   return false;
+}
+
+static bool 
+put_user (uint8_t *udst, uint8_t byte)
+{
+  int error_code;
+  asm("movl $1f, %0; movb %b2, %1; 1:"
+      : "=&a" (error_code), "=m" (*udst) : "r" (byte));
+  return error_code != -1;
 }
