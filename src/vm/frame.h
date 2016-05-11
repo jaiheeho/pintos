178d8c85643d69228e94a07278957d92e8085642
jaihee <jaihee0402@gmail.com>
2016-05-11 21:13:23 +0900

    implementing

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,13 +1,7 @@
 #include <hash.h>
 #include "vm/page.h"
 
-// struct fte {
-//   void* frame_addr;
-//   struct list_elem elem;
-//   struct spte supplement_page;
-// };
-
 void frame_table_init();
 void frame_table_free();
-void* frame_allocate(struct spte *supplement_page);
-int frame_evict(struct spte *supplement_page);
+void* frame_allocate(struct spte*);
+int frame_evict(struct spte *);
