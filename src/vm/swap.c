ab5a40cac6ebfccc85cd4adb94e213706dac6245
jaihee <jaihee0402@gmail.com>
2016-05-11 23:32:41 +0900

    implementing

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -21,8 +21,8 @@ void swap_table_init()
 	sema_init(&swap_lock,1);
 	/* get swap disk */
 	swap_disk = disk_get(1,1);
-	return;
 	swap_size = disk_size(swap_disk)/8;
+	printf("swap_size %d", swap_size);
 	/* get bitmap for swap slots*/
 	swap_bitmap = bitmap_create(swap_size);
 	if (swap_bitmap == NULL)
