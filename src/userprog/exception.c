90d9f316c49e664c1269d6eb30c351a1558a4dd0
jaihee <jaihee0402@gmail.com>
2016-05-12 20:31:40 +0900

    jay implementing

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,12 +148,10 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-
-
   /***** ADDED CODE *****/
   /*Deferencing NULL should be exited instead of killed (test : bad_read)*/
   /*Deferencing addr above 0xC0000000 should be exited instead of killed (test : bad_read)*/
-  //printf("faulted_addr: %0x\n", fault_addr);
+  printf("faulted_addr: %0x\n", fault_addr);
 
   if (fault_addr == NULL || fault_addr >= (void*)0xC0000000
       || fault_addr < (void*)0x08048000 || (!not_present))
