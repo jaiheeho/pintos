00c2dd63f3f1415f5da951022bc929d0508a246c
June Yang <laoconeth@kaist.ac.kr>
2016-05-12 12:29:51 -0700

    12345

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -153,9 +153,10 @@ page_fault (struct intr_frame *f)
   /***** ADDED CODE *****/
   /*Deferencing NULL should be exited instead of killed (test : bad_read)*/
   /*Deferencing addr above 0xC0000000 should be exited instead of killed (test : bad_read)*/
+  printf("faulted_addr: %0x\n", fault_addr);
 
   if (fault_addr == NULL || fault_addr >= (void*)0xC0000000
-      || fault_addr < (void*)0x08048000)
+      || fault_addr < (void*)0x08048000 || (!not_present))
     exit(-1);
   /***** END OF ADDED CODE *****/
 
@@ -167,7 +168,7 @@ page_fault (struct intr_frame *f)
       // stack or heap(or other segment)?
       if(((PHYS_BASE - fault_addr) < STACK_MAX))
   {
-    if(((f->esp - fault_addr) < STACK_STRIDE))
+    if(((f->esp - fault_addr) <= STACK_STRIDE))
       {
 	//printf("GROW : esp = %0x || fault_addr = %0x", f->esp, fault_addr);
 	stack_growth(fault_addr);
