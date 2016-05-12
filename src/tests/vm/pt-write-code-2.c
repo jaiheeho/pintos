15d45d838d74bd55aae15d2a674960afb6e33fd1
jaihee <jaihee0402@gmail.com>
2016-05-12 20:47:13 +0900

    jay implementing

--- src/tests/vm/pt-write-code-2.c
+++ src/tests/vm/pt-write-code-2.c
@@ -8,7 +8,7 @@ void
 test_main (void)
 {
   int handle;
-
+  msg("start\n");
   CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
   read (handle, (void *) test_main, 1);
   fail ("survived reading data into code segment");
