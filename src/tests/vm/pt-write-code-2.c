1ad4d062caf222e1f3e27556695890d8d19fb22d
jaihee <jaihee0402@gmail.com>
2016-05-13 18:52:31 +0900

    fixed

--- src/tests/vm/pt-write-code-2.c
+++ src/tests/vm/pt-write-code-2.c
@@ -10,7 +10,9 @@ test_main (void)
   int handle;
   handle = open ("sample.txt");
   printf("handle: %d", handle);
-  CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
+  CHECK (handle > 1, "open \"sample.txt\"");
+
+  //CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
   read (handle, (void *) test_main, 1);
   fail ("survived reading data into code segment");
 }
