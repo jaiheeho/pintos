3a37669f3d211a4e3a9bf02e29486f082edd0ed5
jaihee <jaihee0402@gmail.com>
2016-05-14 00:27:26 +0900

    asdf

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -343,7 +343,7 @@ int read (int fd, void *buffer, unsigned length)
     // //   }
     // }      
     printf("here1\n");
-    printf("buff: %d", (int)buffer);
+    printf("buff: %d", (uint32_t)buffer);
     retval = file_read(file, buffer, length);
     printf("here2\n");
     sema_up(&filesys_global_lock);
