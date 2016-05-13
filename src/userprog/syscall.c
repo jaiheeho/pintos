074f4ee15482c20a4330ce5503a0cfd4af5f4d5d
jaihee <jaihee0402@gmail.com>
2016-05-13 16:21:10 +0900

    fixed

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -321,8 +321,6 @@ int read (int fd, void *buffer, unsigned length)
       sema_up(&filesys_global_lock);
       return -1;
     }
-    
-    printf("here");
     for (i = 0; i< length ; i++)
     {
       if (!put_user (buf_char + i , 1))
