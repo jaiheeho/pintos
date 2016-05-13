f384c24c5d21ad75081b00e5e064c43d4ec5dbf4
jaihee <jaihee0402@gmail.com>
2016-05-14 00:43:51 +0900

    asdf

--- src/vm/page.c
+++ src/vm/page.c
@@ -111,10 +111,13 @@ int load_page(void* faulted_user_addr)
       
       //install the page in user page table
       install_page(new_spte->user_addr, new_spte->phys_addr, writable);
+      printf("A\n");
       
     }
   else  // page is in spte.(in swap space)
     {
+            printf("B\n");
+
       spte_target = hash_entry(e, struct spte, elem);
       if(pagedir_get_page(thread_current()->pagedir, spte_target->user_addr))
 	{
