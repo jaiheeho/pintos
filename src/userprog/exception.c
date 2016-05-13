56c95d147244ab0f2d146e43ff3ff2eb8f2f74fc
jaihee <jaihee0402@gmail.com>
2016-05-14 03:20:11 +0900

    fixed

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -151,9 +151,9 @@ page_fault (struct intr_frame *f)
   /***** ADDED CODE *****/
   /*Deferencing NULL should be exited instead of killed (test : bad_read)*/
   /*Deferencing addr above 0xC0000000 should be exited instead of killed (test : bad_read)*/
-  printf("faulted_addr: %0x\n", fault_addr);
-  printf("Errorcode : %d %d %d\n", not_present, write, user);
-  printf("tid: %d\n", thread_current()->tid);
+  // printf("faulted_addr: %0x\n", fault_addr);
+  // printf("Errorcode : %d %d %d\n", not_present, write, user);
+  // printf("tid: %d\n", thread_current()->tid);
 
   if (fault_addr == NULL || fault_addr >= (void*)0xC0000000
       || fault_addr < (void*)0x08048000 || (!not_present))
