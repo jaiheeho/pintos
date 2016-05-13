9beb133966400812d3fe9854b24dc8791163d5fc
jaihee <jaihee0402@gmail.com>
2016-05-14 02:21:21 +0900

    asdf

--- src/tests/vm/pt-bad-read.c
+++ src/tests/vm/pt-bad-read.c
@@ -11,7 +11,6 @@ test_main (void)
   int handle;
 
   CHECK ((handle = open ("sample.txt")) > 1, "open \"sample.txt\"");
-  printf("asdf : %0x\n",&handle - 4096);
   read (handle, (char *) &handle - 4096, 1);
   fail ("survived reading data into bad address");
 }
