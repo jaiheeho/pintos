828b19b936e50b47ba434be63ed0e0718f81ffd0
jaihee <jaihee0402@gmail.com>
2016-05-14 02:12:29 +0900

    asdf

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -152,62 +152,62 @@ page_fault (struct intr_frame *f)
   /*Deferencing NULL should be exited instead of killed (test : bad_read)*/
   /*Deferencing addr above 0xC0000000 should be exited instead of killed (test : bad_read)*/
   printf("faulted_addr: %0x\n", fault_addr);
-  //printf("Errorcode : %d %d %d\n", not_present, write, user);
+  printf("Errorcode : %d %d %d\n", not_present, write, user);
 
   if (fault_addr == NULL || fault_addr >= (void*)0xC0000000
       || fault_addr < (void*)0x08048000 || (!not_present))
-    {
+  {
       //printf("fault_addr is naughty : not_present=%d\n", not_present);
-      exit(-1);
-    }
+    exit(-1);
+  }
 
 
   /***** END OF ADDED CODE *****/
   if((not_present) && (user) &&(is_user_vaddr(fault_addr))
      && (fault_addr > (void*)0x08048000))
-    {
-      // valid to load page
+  {
+    // valid to load page
 
-      // stack or heap(or other segment)?
-      if((((uint32_t)PHYS_BASE - (uint32_t)fault_addr) < (uint32_t)STACK_MAX))
-	{
-	  if((((uint32_t)f->esp - (uint32_t)fault_addr) <= (uint32_t)STACK_STRIDE))
+    // stack or heap(or other segment)?
+    if((((uint32_t)PHYS_BASE - (uint32_t)fault_addr) < (uint32_t)STACK_MAX))
+    {
+      if((((uint32_t)f->esp - (uint32_t)fault_addr) <= (uint32_t)STACK_STRIDE))
 	    {
 	      //printf("STACK_MAX = %d, STACK_STRID = %d\n", STACK_MAX, STACK_STRIDE);
 	      printf("GROW : esp = %0x || fault_addr = %0x", f->esp, fault_addr);
 
 	      stack_growth(fault_addr);
 	    }
-	  else
+      else
 	    {
 	      if((uint32_t)f->esp > (uint32_t)fault_addr)
-		{
-		  // it is stack. but stride aint right
-		  //printf("page fault handler: stack stride problem. esp = %0x, fault_addr = %0x\n", f->esp, fault_addr);
-		  exit(-1);
-		}
-	      else 
-		{  
-		  // swap in the swapped out stack page
-		  if(!load_page(fault_addr))
 		    {
-		      PANIC("load page failed.");
-		    } 
-		}
-	      
-	    }
-	}
-      else
-	{
-	  //printf("faulted_addr2: %0x\n", fault_addr);
-
-	  if(!load_page(fault_addr))
-	    {
-	      PANIC("load page failed.");
-	    }
-	}
-      
+    		  // it is stack. but stride aint right
+    		  //printf("page fault handler: stack stride problem. esp = %0x, fault_addr = %0x\n", f->esp, fault_addr);
+		      exit(-1);
+		    }
+	      else 
+		    {  
+          printf("load page in page_fault");
+		      // swap in the swapped out stack page
+          if(!load_page(fault_addr))
+		      {
+		        PANIC("load page failed.");
+		      } 
+		    }
+      }
     }
+    else
+    {
+      printf("faulted_addr2: %0x\n", fault_addr);
+
+	    if(!load_page(fault_addr))
+      {
+	       PANIC("load page failed.");
+       }
+    }
+      
+  }
 
   printf("page fault handler: end\n");
   
