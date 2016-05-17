93cdf96740d1aa477003d8071d7bbb111dd0dc52
jaihee <jaihee0402@gmail.com>
2016-05-18 00:24:18 +0900

    imple of jay

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -152,7 +152,7 @@ page_fault (struct intr_frame *f)
   /*Deferencing NULL should be exited instead of killed (test : bad_read)*/
   /*Deferencing addr above 0xC0000000 should be exited instead of killed (test : bad_read)*/
   printf("faulted_addr: %0x\n", fault_addr);
-  printf("f->esp : %0x\n", fault_addr);
+  printf("f->esp : %0x\n", f->esp);
   printf("Errorcode : %d %d %d\n", not_present, write, user);
   // printf("tid: %d\n", thread_current()->tid);
   
