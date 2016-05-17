19a26a3fb53a6a1df1c5ee7c8dbbbac067d38c8d
jaihee <jaihee0402@gmail.com>
2016-05-18 00:31:34 +0900

    imple of jay

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -175,9 +175,12 @@ page_fault (struct intr_frame *f)
   /* check whether invalid by exceeding STACK_MAX*/
   if( (uint32_t)PHYS_BASE - (uint32_t)fault_addr >= (uint32_t)STACK_MAX )
   {
+    printf("here")
     if(!load_page(fault_addr))
-     PANIC("Exceeded STACK_MAX");
+      PANIC("Exceeded STACK_MAX");
+    return;
   }
+
 
   /* not_present | read | kernel = 110*/
   if ( not_present && !write && !user)
