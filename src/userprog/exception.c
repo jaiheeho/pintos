260ede72de9619a55b55cd658ca0b7af07f0185f
jaihee <jaihee0402@gmail.com>
2016-05-22 21:53:31 +0900

    now restart

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -150,9 +150,9 @@ page_fault (struct intr_frame *f)
   
   /***** ADDED CODE *****/
   // printf("----------------------------------------------\n");
-  printf("faulted_addr: %0x\n", fault_addr);
-  printf("f->esp : %0x\n", f->esp);
-  printf("Errorcode : %d %d %d\n", not_present, write, user);
+  // printf("faulted_addr: %0x\n", fault_addr);
+  // printf("f->esp : %0x\n", f->esp);
+  // printf("Errorcode : %d %d %d\n", not_present, write, user);
   // printf("tid: %d\n", thread_current()->tid);
   
   /* this case is for invalid fauled_addr exit process and release the lock if thread has lock
