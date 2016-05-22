1441d181fee780ac9fe735af7ba29c528ed490d8
jaihee <jaihee0402@gmail.com>
2016-05-22 16:14:44 +0900

    now restart

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -641,7 +641,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
   ASSERT (pg_ofs (upage) == 0);
   ASSERT (ofs % PGSIZE == 0);
   //printf("ofs=%d upage=%p, read_bytes=%d, zero_bytes=%d\n", ofs, upage, read_bytes, zero_bytes);
-  //file_seek (file, ofs);
+  file_seek (file, ofs);
   while (read_bytes > 0 || zero_bytes > 0) 
     {
       /* Do calculate how to fill this page.
