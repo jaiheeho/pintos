14e9f19689586599d8e8c3350650960dab8b7f89
jaihee <jaihee0402@gmail.com>
2016-05-22 21:11:24 +0900

    now restart

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -15,7 +15,6 @@ static struct list frame_table;
 static struct semaphore frame_table_lock;
 static struct list_elem *clock_head; 
 
-
 void frame_free_nolock(struct fte* fte_to_free);
 
 /************************************************************************
@@ -26,7 +25,6 @@ void frame_table_init()
 {
   list_init(&frame_table);
   sema_init(&frame_table_lock,1);
-  clock_head = list_head(&frame_table);
 }
 
 /************************************************************************
@@ -46,8 +44,6 @@ void frame_table_free()
     palloc_free_page(frame_entry);
   }
   sema_up(&frame_table_lock);
-
-
 }
 
 /************************************************************************
@@ -65,7 +61,7 @@ void* frame_allocate(struct spte* supplement_page)
 
   while(1)
   {
-      printf("here2-0-2\n");
+    printf("here2-0-2\n");
 
     new_frame = palloc_get_page(PAL_USER | PAL_ZERO);
     if(new_frame == NULL)
@@ -78,11 +74,10 @@ void* frame_allocate(struct spte* supplement_page)
     }
     else
     {
-              printf("here2-0-4\n");
+        printf("here2-0-4\n");
 
       // frame allocation succeeded; add to frame table
       struct fte* new_fte_entry = (struct fte*)malloc(sizeof(struct fte));
-
       if(new_fte_entry == NULL)
     	{
     	   printf("cannot allocate new fte!!\n");
@@ -101,6 +96,9 @@ void* frame_allocate(struct spte* supplement_page)
       
       // insert into frame table
       list_push_back(&frame_table, &new_fte_entry->elem);
+      if (list_empty(&frame_table))
+        clock_head = list_head(&frame_table);
+
               printf("here2-0-5\n");
 
       break;
@@ -177,45 +175,38 @@ void frame_evict()
   void* new;
 
   //printf("frame_evict:\n");
-    printf("here2-0-3-0\n");
-
+  printf("here2-0-3-0\n");
 
   //start from the beginning of table.
+  for (iter = clock_head /*list_begin(&frame_table)*/;;)
+  {
+    frame_entry= list_entry(iter, struct fte, elem);
+    struct spte *paired_spte = (struct spte*)frame_entry->supplement_page;
 
-  for (iter = list_begin(&frame_table);;)
-    {
-      frame_entry= list_entry(iter, struct fte, elem);
-      struct spte *paired_spte = (struct spte*)frame_entry->supplement_page;
-
-      //if(paired_spte->user_addr >= (void*)0xb0000000)
-	//printf("user_addr: %0x\n", paired_spte->user_addr);
+    //if(paired_spte->user_addr >= (void*)0xb0000000
+    //printf("user_addr: %0x\n", paired_spte->user_addr);
     printf("here2-0-3-1\n");
 
-      if(paired_spte->frame_locked == false)
-	{
-	  
-	  if(pagedir_is_accessed(frame_entry->thread->pagedir,
-				 paired_spte->user_addr) == true)
+    if(paired_spte->frame_locked == false)
+  	{
+  	  if(pagedir_is_accessed(frame_entry->thread->pagedir, paired_spte->user_addr) == true)
 	    {
-	      pagedir_set_accessed(frame_entry->thread->pagedir,
-				   paired_spte->user_addr, false);
+	      pagedir_set_accessed(frame_entry->thread->pagedir, paired_spte->user_addr, false);
 	    }
-	  else
-	    {
-	      // not recently used. commence eviction
-	      break;
-	    }
-	
-	}
-          printf("here2-0-3-2\n");
+  	  else
+      {
+        // not recently used. commence eviction
+        break;
+      }
+  	}
+    printf("here2-0-3-2\n");
 
-      iter = list_next(iter);
-      if(iter == list_end(&frame_table))
-	{
-	  iter = list_begin(&frame_table);
-	}  
-      
-    }
+    iter = list_next(iter);
+    if(iter == list_end(&frame_table))
+  	{
+  	  iter = list_begin(&frame_table);
+  	}  
+  }
   
   frame_entry= list_entry(iter, struct fte, elem);
   supplement_page = (struct spte*)frame_entry->supplement_page;
@@ -225,6 +216,16 @@ void frame_evict()
   supplement_page->present = false;  
   //detach fte from frame table list
   list_remove(&frame_entry->elem);
+  if (!list_empty(&frame_table))
+  {
+    if(list_next(&frame_table) == list_end(&frame_table))
+      clock_head = list_begin(&frame_table);
+    else
+      clock_head = list_next(iter);
+  }
+
+
+
   //detach frame from spte (this is for ensurance)
   pagedir_clear_page(frame_entry->thread->pagedir, ((struct spte *)frame_entry->supplement_page)->user_addr);
 
