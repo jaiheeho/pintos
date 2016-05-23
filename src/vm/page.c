e406c0e9ca23c537e46623ea6c94355f014dfd92
jaihee <jaihee0402@gmail.com>
2016-05-23 15:38:27 +0900

    jay imple

--- src/vm/page.c
+++ src/vm/page.c
@@ -59,8 +59,8 @@ static void spte_destroyer_func(struct hash_elem *e, void *aux)
     }
   else
     {
+      swap_free_slot(target->swap_idx);
       // 1) free swap slot
-      //swap_free_slot(target->swap_idx);
     }
   // 3) free spte
   free(target);
