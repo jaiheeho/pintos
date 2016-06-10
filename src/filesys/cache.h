659736e65d899fd496a7b59683432543e1dc9e8e
jaihee <jaihee0402@gmail.com>
2016-06-11 01:23:46 +0900

    jayu

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -2,8 +2,7 @@
 #define FILESYS_CACHE_H
 
 
-typedef uint32_t disk_sector_t;
-
+#include "devices/disk.h"
 void buffer_cache_init(void);
 int buffer_cache_read(disk_sector_t , char *, size_t, int);
 int buffer_cache_write(disk_sector_t , char *, size_t, int);
