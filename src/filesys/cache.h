705cb128c1428fa0a1c04e2b45e09165a70892a3
jaihee <jaihee0402@gmail.com>
2016-06-11 01:22:09 +0900

    jayu

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -2,8 +2,8 @@
 #define FILESYS_CACHE_H
 
 void buffer_cache_init(void);
-int buffer_cache_read(disk_sector_t, void *, size_t, int);
-int buffer_cache_write(disk_sector_t, void *, size_t, int);
+int buffer_cache_read(disk_sector_t , char *, size_t, int);
+int buffer_cache_write(disk_sector_t , char *, size_t, int);
 void buffer_cache_elem_free(disk_sector_t);
 void buffer_cache_free(void);
 
