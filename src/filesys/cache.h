c9796a42f01a6f55ee799d7965c060c35a31af86
jaihee <jaihee0402@gmail.com>
2016-06-11 00:51:52 +0900

    jay

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "devices/disk.h"
 
 
 void buffer_cache_init();
-int buffer_cache_read(disk_sector_t sector, char* buffer, size_t size, int off);
-int buffer_cache_write(disk_sector_t sector, char* buffer, size_t size, int off);
-
-void buffer_cache_elem_free(disk_sector_t sector);
+int buffer_cache_read(disk_sector_t, char*, size_t, int);
+int buffer_cache_write(disk_sector_t, char*, size_t, int);
+void buffer_cache_elem_free(disk_sector_t);
 void buffer_cache_free();
