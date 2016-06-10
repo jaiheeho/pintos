b3d307478b0d9e8627c709a650ad55d136c97c31
jaihee <jaihee0402@gmail.com>
2016-06-11 01:14:27 +0900

    jayu

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -1,13 +1,16 @@
 #ifndef FILESYS_CACHE_H
 #define FILESYS_CACHE_H
-#include <stdio.h>
-#include <stdlib.h>
+
 #include "devices/disk.h"
 
 void buffer_cache_init(void);
-int buffer_cache_read(disk_sector_t, void*, size_t, int);
-int buffer_cache_write(disk_sector_t, void*, size_t, int);
+int buffer_cache_read(disk_sector_t, void *, size_t, int);
+int buffer_cache_write(disk_sector_t, void *, size_t, int);
 void buffer_cache_elem_free(disk_sector_t);
 void buffer_cache_free(void);
 
-#endif
+/* Opening and closing files. */
+struct file *file_open (struct inode *);
+
+
+#endif /* filesys/file.h */
