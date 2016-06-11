69e8327ed1793929d787282a9d1ac3da9154f245
jaihee <jaihee0402@gmail.com>
2016-06-11 23:56:53 +0900

    jay

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -7,7 +7,7 @@ int buffer_cache_read(disk_sector_t , char *, size_t, int);
 int buffer_cache_write(disk_sector_t , char *, size_t, int);
 void buffer_cache_elem_free(disk_sector_t);
 void buffer_cache_free(void);
-void buffer_cache_flush(void):
+void buffer_cache_flush(void);
 
 extern bool buffer_cache_inited;
 
