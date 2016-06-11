dd06be224746cd7d0faa5f63a6a8ffdcaa5dac16
jaihee <jaihee0402@gmail.com>
2016-06-11 22:28:56 +0900

    jay

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -152,15 +152,9 @@ inode_open (disk_sector_t sector)
   inode->open_cnt = 1;
   inode->deny_write_cnt = 0;
   inode->removed = false;
-  if (buffer_cache_inited == false)
-  {
+  if(!buffer_cache_read(inode->sector, (char *)&inode->data, DISK_SECTOR_SIZE, 0)
     disk_read (filesys_disk, inode->sector, &inode->data);
-    return inode;
-  }
-  else
-  {
-    buffer_cache_read(inode->sector, (char *)&inode->data, DISK_SECTOR_SIZE, 0);
-  }
+  return inode;
 }
 
 /* Reopens and returns INODE. */
