1c8ba03869c92dc8986f7cbc2d0cc9cbfec0e786
jaihee <jaihee0402@gmail.com>
2016-06-17 19:11:58 +0900

    jay

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -176,7 +176,8 @@ bool inode_free_map_add(size_t size, off_t pos, struct inode_disk *disk_inode)
   printf("size : %d new size : %d\n", size, pos);
   printf("AT ADD; length : %d, indirect_size; %d, direct_size:%d \n",
     length, indirect_size, direct_size);
-
+  printf("AT ADD end; length : %d , indirect_size; %d, direct_size:%d \n",
+  _length, _indirect_size, _direct_size);
 
 
   for (i = indirect_size-1 ; i < _indirect_size; i ++)
